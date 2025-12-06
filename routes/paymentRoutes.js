4a2ffb73c9106c090eb1d4252af14c573bd81d82
aaftab343 <56512075+aaftab343@users.noreply.github.com>
2025-12-06 16:06:53 +0530

    Update paymentRoutes.js

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -1,11 +1,14 @@
-import express from 'express';
-import { createOrder, verifyPayment, adminMarkPaymentPaid } from '../controllers/paymentController.js';
-import { protect } from '../middleware/authMiddleware.js';
+import express from "express";
+import {
+  createOrder,
+  verifyPayment,
+} from "../controllers/paymentController.js";
+
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post('/create-order', protect(['user', 'superAdmin', 'admin', 'staffAdmin']), createOrder);
-router.post('/verify', verifyPayment);
-router.put('/:id/manual-paid', protect(['superAdmin', 'admin']), adminMarkPaymentPaid);
+router.post("/create-order", protect(["user"]), createOrder);
+router.post("/verify", verifyPayment);
 
 export default router;
