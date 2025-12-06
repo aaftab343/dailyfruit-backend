706949423f0b8a68f572cbc1fe8a8bed8252c184
aaftab343 <56512075+aaftab343@users.noreply.github.com>
2025-12-06 16:20:09 +0530

    Update paymentRoutes.js

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -2,13 +2,19 @@ import express from "express";
 import {
   createOrder,
   verifyPayment,
+  getMyPayments,
+  getLatestInvoice
 } from "../controllers/paymentController.js";
 
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/create-order", protect(["user"]), createOrder);
-router.post("/verify", verifyPayment);
+router.post("/create-order", protect(['user']), createOrder);
+router.post("/verify", protect(['user']), verifyPayment);
+
+// ⭐ NEW ENDPOINTS
+router.get("/my-payments", protect(['user']), getMyPayments);
+router.get("/latest-invoice", protect(['user']), getLatestInvoice);
 
 export default router;
