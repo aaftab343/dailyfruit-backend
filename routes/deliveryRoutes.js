a9829065422f4d3390dd8f88990eaf2747f9fd93
aaftab343 <56512075+aaftab343@users.noreply.github.com>
2025-12-07 11:59:52 +0530

    Update deliveryRoutes.js

--- routes/deliveryRoutes.js
+++ routes/deliveryRoutes.js
@@ -1,12 +1,21 @@
 import express from 'express';
-import { createDelivery, updateDeliveryStatus, getDeliveries, getMyDeliveries } from '../controllers/deliveryController.js';
+import {
+  createDelivery,
+  updateDeliveryStatus,
+  getDeliveries,
+  getMyDeliveries
+} from '../controllers/deliveryController.js';
+
 import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
+// ADMIN ROUTES
 router.post('/', protect(['superAdmin', 'admin', 'staffAdmin']), createDelivery);
 router.put('/:id', protect(['superAdmin', 'admin', 'staffAdmin']), updateDeliveryStatus);
 router.get('/', protect(['superAdmin', 'admin', 'staffAdmin']), getDeliveries);
+
+// USER ROUTE
 router.get('/me', protect(['user', 'superAdmin', 'admin', 'staffAdmin']), getMyDeliveries);
 
 export default router;
