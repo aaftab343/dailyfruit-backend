6bae06b9455397d1232474c5b2a2e8f10617b4d1
aaftab343 <56512075+aaftab343@users.noreply.github.com>
2025-12-14 15:29:30 +0530

    Update Admin.js

--- models/Admin.js
+++ models/Admin.js
@@ -4,7 +4,18 @@ const adminSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true, lowercase: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ['superAdmin', 'admin', 'staffAdmin'], default: 'admin' }
+
+  role: {
+    type: String,
+    enum: [
+      'SUPER_ADMIN',
+      'MANAGER',
+      'DELIVERY_ADMIN',
+      'SUPPORT'
+    ],
+    default: 'SUPPORT'
+  }
+
 }, { timestamps: true });
 
 const Admin = mongoose.model('Admin', adminSchema);
