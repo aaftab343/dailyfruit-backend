b1bf5264eed0d0799dff2299c96f11981ddde8f5
aaftab343 <56512075+aaftab343@users.noreply.github.com>
2025-12-14 17:51:19 +0530

    Update adminDeliveryController.js

--- controllers/adminDeliveryController.js
+++ controllers/adminDeliveryController.js
@@ -3,10 +3,10 @@ import Delivery from "../models/Delivery.js";
 export const getTodayDeliveries = async (req, res) => {
   try {
     const start = new Date();
-    start.setHours(0, 0, 0, 0);
+    start.setHours(0,0,0,0);
 
     const end = new Date();
-    end.setHours(23, 59, 59, 999);
+    end.setHours(23,59,59,999);
 
     const deliveries = await Delivery.find({
       deliveryDate: { $gte: start, $lte: end }
