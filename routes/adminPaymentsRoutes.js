4093e3d30a0aa38ddfbbc547f29371a964f0e79f
aaftab343 <56512075+aaftab343@users.noreply.github.com>
2025-12-14 17:50:08 +0530

    Update adminPaymentsRoutes.js

--- routes/adminPaymentsRoutes.js
+++ routes/adminPaymentsRoutes.js
@@ -1,9 +1,16 @@
-  import express from "express";
+import express from "express";
 import { protectAdmin } from "../middleware/adminAuthMiddleware.js";
 import { getAdminPayments } from "../controllers/adminPaymentsController.js";
 
 const router = express.Router();
 
-router.get("/", protectAdmin(["SUPER_ADMIN","MANAGER"]), getAdminPayments);
+/**
+ * GET /api/admin/payments
+ */
+router.get(
+  "/",
+  protectAdmin(["SUPER_ADMIN", "MANAGER"]),
+  getAdminPayments
+);
 
 export default router;
