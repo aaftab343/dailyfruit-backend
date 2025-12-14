8e9fe5d92baa998c105c3d0111891eff5a07d95a
aaftab343 <56512075+aaftab343@users.noreply.github.com>
2025-12-14 16:02:00 +0530

    Update adminOrdersController.js

--- controllers/adminOrdersController.js
+++ controllers/adminOrdersController.js
@@ -1,1 +1,16 @@
+import Subscription from "../models/Subscription.js";
+
+export const getAdminOrders = async (req, res) => {
+  try {
+    const orders = await Subscription.find()
+      .populate("user", "name email phone address")
+      .populate("plan", "name price")
+      .sort({ createdAt: -1 });
+
+    res.json(orders);
+  } catch (err) {
+    console.error("getAdminOrders error:", err.message);
+    res.status(500).json({ message: "Server error" });
+  }
+};
 
